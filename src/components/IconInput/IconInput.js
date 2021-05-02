4fc73466a09c75a6b60482d28c5464cb94f50aed
ljil;kj <schoeps.pat@gmail.com>
2021-05-02 12:48:22 -0500

    submission

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,21 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  small: {
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24,
+  },
+  large: {
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36,
+  },
+};
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +28,66 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const styles = STYLES[size];
+  return (
+    <Wrapper>
+      <VisuallyHidden><label for="text-input">label</label></VisuallyHidden>
+
+      <Input 
+        type="text" 
+        id="text-input" 
+        placeholder={placeholder}
+        style={{
+          '--width': width + 'px',
+          '--height': styles.height / 16 + 'rem',
+          '--border-thickness': styles.borderThickness + 'px',
+          '--font-size': styles.fontSize / 16 + 'rem',
+        }}
+      />
+      <StyledIcon style={{ '--size': styles.iconSize + 'px' }}>
+        <Icon id={icon} strokeWidth={1} size={styles.iconSize} />
+      </StyledIcon>
+
+    </Wrapper>
+  )
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  display: block;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+
+const Input = styled.input`
+  width: var(--width);
+  height: var(--height);
+  font-size: var(--font-size);
+  border: none;
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
+  padding-left: var(--height);
+  color: inherit;
+  font-weight: 700;
+  outline-offset: 2px;
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`
+
+const StyledIcon = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`
+
+
+
+
 
 export default IconInput;
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,63 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    "--borderRadius": 4 + "px",
+    "--height": 8 + "px",
+    "--padding": 0 + "px",
+  },
+  medium: {
+    "--borderRadius": 4 + "px",
+    "--height": 12 + "px",
+  },
+  large: {
+    "--borderRadius": 4 + "px",
+    "--height": 24 + "px",
+    "--padding": 4 + "px",
+  }
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size];
+
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+  }
+
+  return (
+    <Wrapper style={styles} role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100">
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <Overflow>
+        <Inner style={{width: `${value}%`}}></Inner>
+      </Overflow>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: 370px;
+  background: ${COLORS.transparentGray15};
+  height: var(--height);
+  padding: var(--padding);
+  border-radius: var(--borderRadius);
+  overflow: hidden;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`
+
+const Overflow = styled.div`
+  display: block;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  border-radius: var(--borderRadius);
+`
+
+const Inner = styled.div`
+  background: ${COLORS.primary};
+  height: 100%;
+  display: block;
+`
+
 
 export default ProgressBar;
